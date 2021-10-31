db4d5158222d9debc8c050ac10051894466e148d
Rebeca Baptista Fonseca Viana <re.bfviana@gmail.com>
2021-10-31 15:22:46 -0300

    Adiciona resultado IMC no HTML
    Esse commit modifica o blog.html para mostrar o resultado do IMC no
    corpo da página:
    	- adiciona uma div
    	- adiciona uma strong
    	- altera calculaImc.js para exibir e ocultar a div
    
    Closes #1

--- calculaImc.js
+++ calculaImc.js
@@ -1,20 +1,37 @@
 peso.focus();
 altura.focus();
 
+var visibilidade = true; //Variável que vai manipular o botão Exibir
+document.getElementById("dvConteudo").style.display = "none";
+
 function calculaImc()
 {
-    var pesoCalc = parseInt (document.getElementById("peso").value);
-    var alturaCalc = parseInt (document.getElementById("altura").value);
-    var imc = pesoCalc/(alturaCalc*alturaCalc);
-    alert("Seu IMC é:" + imc);
+    var pesoCalc = parseFloat (document.getElementById("peso").value);
+    var alturaCalc = parseFloat (document.getElementById("altura").value);
+    alturaCalc = alturaCalc/100; //Transformando cm em metros
+    var imc = parseFloat (pesoCalc/(alturaCalc*alturaCalc));
+    imc = imc.toFixed(1); //Determinando o número de casas decimais (função arredonda para cima)
+    document.getElementsByClassName("resultadoImc")[0].innerHTML  = imc;
+
     
     // Melhorando a usabilidade e limpando o input
     peso.value = "";
     peso.focus();
     altura.value = "";
     altura.focus();
+  
+    if (visibilidade) {//Se a variável visibilidade for igual a true, então...
+        document.getElementById("dvConteudo").style.display = "block";//Exibimos a div
+        visibilidade = false;//alteramos o valor da variável para falso.
+    }
+    /* Se caso precisasse desabilitar a div:
+    else { //ou se a variável estiver com o valor false..
+        document.getElementById("dvConteudo").style.display = "none";//Ocutamos a div..
+        visibilidade = true;//Alteramos o valor da variável para true.
+    //}*/
 }
 
 
 var button = document.getElementById("buttonImc");
 button.onclick = calculaImc;
+
